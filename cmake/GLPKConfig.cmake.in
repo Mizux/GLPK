38cf669d67603bb1740cb7d147224405b4303f17
Mizux Seiha <mizux.dev@gmail.com>
2021-11-10 17:46:14 +0100

    Add CMake support

---
+++ cmake/GLPKConfig.cmake.in
@@ -0,0 +1,6 @@
+# GLPK CMake configuration file
+
+#include(CMakeFindDependencyMacro)
+#find_dependency(Foo REQUIRED NO_MODULE)
+
+include("${CMAKE_CURRENT_LIST_DIR}/GLPKTargets.cmake")
---
+++ cmake/c.cmake
@@ -0,0 +1,95 @@
+enable_language(C)
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_C_STANDARD_REQUIRED 11)
+set(CMAKE_C_EXTENSIONS ON)
+
+include(GNUInstallDirs)
+
+# add_c_test()
+# CMake function to generate and build C++ test.
+# Parameters:
+#  the C++ filename
+# e.g.:
+# add_c_test(foo.c)
+function(add_c_test FILE_NAME)
+  message(STATUS "Configuring test ${FILE_NAME}: ...")
+  get_filename_component(TEST_NAME ${FILE_NAME} NAME_WE)
+  get_filename_component(TEST_DIR ${FILE_NAME} DIRECTORY)
+  get_filename_component(COMPONENT_DIR ${FILE_NAME} DIRECTORY)
+  get_filename_component(COMPONENT_NAME ${COMPONENT_DIR} NAME)
+
+  if(APPLE)
+    set(CMAKE_INSTALL_RPATH
+      "@loader_path/../${CMAKE_INSTALL_LIBDIR};@loader_path")
+  elseif(UNIX)
+    set(CMAKE_INSTALL_RPATH "$ORIGIN/../${CMAKE_INSTALL_LIBDIR}:$ORIGIN")
+  endif()
+
+  add_executable(${TEST_NAME} ${FILE_NAME})
+  target_include_directories(${TEST_NAME} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
+  target_compile_features(${TEST_NAME} PRIVATE c_std_11)
+  target_link_libraries(${TEST_NAME} PRIVATE
+    ${PROJECT_NAMESPACE}::GLPK)
+
+  if(BUILD_TESTING)
+    add_test(NAME c_${COMPONENT_NAME}_${TEST_NAME} COMMAND ${TEST_NAME})
+  endif()
+  message(STATUS "Configuring test ${FILE_NAME}: ...DONE")
+endfunction()
+
+add_subdirectory(src)
+
+# Install
+install(EXPORT ${PROJECT_NAME}Targets
+  NAMESPACE ${PROJECT_NAMESPACE}::
+  DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}
+  COMPONENT Devel)
+include(CMakePackageConfigHelpers)
+configure_package_config_file(cmake/${PROJECT_NAME}Config.cmake.in
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+  INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+  NO_SET_AND_CHECK_MACRO
+  NO_CHECK_REQUIRED_COMPONENTS_MACRO)
+write_basic_package_version_file(
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+  COMPATIBILITY SameMajorVersion)
+install(
+  FILES
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+  COMPONENT Devel)
+
+# add_c_example()
+# CMake function to generate and build C++ example.
+# Parameters:
+#  the C++ filename
+# e.g.:
+# add_c_example(foo.c)
+function(add_c_example FILE_NAME)
+  message(STATUS "Configuring example ${FILE_NAME}: ...")
+  get_filename_component(EXAMPLE_NAME ${FILE_NAME} NAME_WE)
+  get_filename_component(COMPONENT_DIR ${FILE_NAME} DIRECTORY)
+  get_filename_component(COMPONENT_NAME ${COMPONENT_DIR} NAME)
+
+  if(APPLE)
+    set(CMAKE_INSTALL_RPATH
+      "@loader_path/../${CMAKE_INSTALL_LIBDIR};@loader_path")
+  elseif(UNIX)
+    set(CMAKE_INSTALL_RPATH "$ORIGIN/../${CMAKE_INSTALL_LIBDIR}:$ORIGIN")
+  endif()
+
+  add_executable(${EXAMPLE_NAME} ${FILE_NAME})
+  target_include_directories(${EXAMPLE_NAME} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
+  target_compile_features(${EXAMPLE_NAME} PRIVATE c_std_11)
+  target_link_libraries(${EXAMPLE_NAME} PRIVATE
+    ${PROJECT_NAMESPACE}::GLPK)
+
+  include(GNUInstallDirs)
+  install(TARGETS ${EXAMPLE_NAME})
+
+  if(BUILD_TESTING)
+    add_test(NAME c_${COMPONENT_NAME}_${EXAMPLE_NAME} COMMAND ${EXAMPLE_NAME})
+  endif()
+  message(STATUS "Configuring example ${FILE_NAME}: ...DONE")
+endfunction()
